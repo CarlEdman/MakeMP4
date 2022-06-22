684f5fe567c9d7525cb06f94e46762cb34c889a3
Carl Edman <CarlEdman@gmail.com>
2022-06-22 15:59:23 -0700

    Many bug fixes
    Move get_meta_mp4info to tagmp4, use get_meta_mutagen instead

--- mp4infos.py
+++ mp4infos.py
@@ -6,55 +6,38 @@ author='Carl Edman (CarlEdman@gmail.com)'
 desc='Tabular list of mp4 metadata in directory and all subdirectories.'
 
 import argparse
-import codecs
 import logging
 import logging.handlers
 import os
 import os.path
-import re
-import shlex
-import subprocess
 import sys
+import csv
 
 from cetools import *
+from tagmp4 import *  # pylint: disable=unused-wildcard-import
 
 parser = None
 args = None
 log = logging.getLogger()
 
-#sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
-
 def main():
-  cats=['Filename']
-  tcats=[]
-  vals=[]
-
-  for root, dirs, files in os.walk(args.directory):
+  vals = []
+  for root, _, files in os.walk(args.directory):
     for file in sorted(files):
       if os.path.splitext(file)[1] not in ['.m4a', '.mp4', '.m4r', '.m4b']: continue
       f=os.path.join(root,file)
-      log.debug('Processing "' + f + '"')
-      vals.append(dict({'Filename':f}))
-      try:
-        mi = subprocess.check_output(['mp4info',f])
-      except subprocess.CalledProcessError:
-        continue
-      for l in mi.decode(errors='ignore').splitlines():
-        if m := re.fullmatch(r'\s+(.+?)\s*:\s*(.+?)\s*',l):
-          if m[1] not in cats: cats.append(m[1])
-          vals[-1][m[1]]=m[2]
-        if m := re.fullmatch(r'(\d+)\s+(\w+)\s*(.*)',l):
-          type='TrackType'+m[1]
-          if type not in tcats: tcats.append(type)
-          vals[-1][type]=m[2]
-          info='TrackInfo'+m[1]
-          if info not in tcats: tcats.append(info)
-          vals[-1][info]=m[3]
+      log.info(f'Processing "{f}"')
+      ret = get_meta_mutagen(f)
+      ret['Filename'] = f
+      vals.append(ret)
 
-  print('\t'.join(cats + tcats))
-  for v in vals:
-    print('\t'.join([v[c] if c in v else '' for c in (cats+tcats)]))
-
+  cats = set(k for v in vals for k in v.keys())
+  if len(cats)==0:
+    log.warning('No valid mp4info produced.')
+    return
+  writer = csv.DictWriter(sys.stdout, fieldnames=cats)
+  writer.writeheader()
+  writer.writerows(vals)
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(fromfile_prefix_chars='@',prog=prog,epilog='Written by: '+author)
@@ -74,4 +57,5 @@ if __name__ == "__main__":
   slogger.setFormatter(logging.Formatter('[%(levelname)s] %(asctime)s: %(message)s'))
   log.addHandler(slogger)
 
+  sys.stdout.reconfigure(encoding='utf-8')
   main()
